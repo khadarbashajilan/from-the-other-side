f9f933dfc134b808f917c951617b974523445cc4
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-11-12 07:28:06 +0530

    added html content in server

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ import http from "node:http";
 const PORT=1024
 
 const server = http.createServer(async (req, res)=>{
-    res.write("HI, welcome to my server\n")
-    res.end("BYE")
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><h1>The server is working</h1></html>')
 })
 
 server.listen(PORT, ()=>{
     console.log(`Server running at :  http://localhost:${PORT}/api`)
-})
+})  
