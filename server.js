0b760f73e76d09420ed0fd3b5fb45759d851aada
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-11-13 07:58:04 +0530

    absolute and relative path of a files

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import http from "node:http";
-
+import { serveStatic } from "./utils/servestatic.js";
 const PORT=1024
 
+// The directory name of the current module
+const __dir = import.meta.dirname
+
 const server = http.createServer(async (req, res)=>{
+    // // Construct the absolute path to the index.html file by joining the directory name with 'public' and 'index.html'
+    // const abspath2res = path.join(import.meta.dirname, 'public', 'index.html')
+    // console.log('absolute: ',abspath2res)
+
+    // // Construct the relative path to the index.html file by joining 'public' and 'index.html'
+    // const relpath2res = path.join('public', 'index.html')
+    // console.log('relative: ',relpath2res)
+
+    serveStatic(__dir)
+
     res.statusCode = 200
     res.setHeader('Content-Type', 'text/html')
     res.end('<html><h1>The server is working</h1></html>')
