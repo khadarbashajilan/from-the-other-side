cfc3636e1688465ad3894d382bc12cc8e8a6f079
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-11-19 06:48:43 +0530

    handle GET request (API & Frontend)

---
+++ handlers/routeHandlers.js
@@ -0,0 +1,14 @@
+// Import the 'getdata' function from the '../utils/getdata.js' module
+import { getdata } from "../utils/getdata.js";
+// Import the 'sendResponse' function from the '../utils/sendResponse.js' module
+import { sendResponse } from "../utils/sendResponse.js";
+
+// Define an asynchronous function named 'handleGet' that takes a 'res' parameter
+export async function handleGet(res) {
+    // Call the 'getdata' function asynchronously to fetch data
+    let data = await getdata()
+    // Convert the fetched data to a JSON string
+    data = JSON.stringify(data)
+    // Call the 'sendResponse' function to send the JSON data as a response with a status code of 200 and a content type of 'application/json'
+    sendResponse(res, 200, 'application/json', data)
+}
--- server.js
+++ server.js
@@ -1,18 +1,32 @@
+// Import the 'http' module from Node.js to create an HTTP server
 import http from "node:http";
+// Import the 'serveStatic' function from the './utils/serveStatic.js' module to serve static files
 import { serveStatic } from "./utils/serveStatic.js";
+// Import the 'handleGet' function from the './handlers/routeHandlers.js' module to handle GET requests to the '/api' endpoint
+import { handleGet } from "./handlers/routeHandlers.js";
 
-const PORT=1024
+// Define the port number on which the server will listen
+const PORT = 1024;
 
 // The directory name of the current module
-const __dir = import.meta.dirname
+const __dir = import.meta.dirname;
 
-// Create an HTTP server
-const server = http.createServer(async (req, res)=>{
-    await serveStatic(req, res, __dir)
+// Create an HTTP server using the 'http.createServer' method
+const server = http.createServer(async (req, res) => {
+    // Check if the request URL is '/api'
+    if (req.url === '/api') {
+        // If the request method is 'GET', call the 'handleGet' function asynchronously
+        if (req.method === 'GET') return await handleGet(res)
+    }
+    // Check if the request URL does not start with '/api'
+    else if (!req.url.startsWith('/api')) {
+        // If the request URL does not start with '/api', call the 'serveStatic' function asynchronously to serve static files
+        return await serveStatic(req, res, __dir)
+    }
 })
 
-// Start the server and listen on the specified port`
-server.listen(PORT, ()=>{
+// Start the server and listen on the specified port
+server.listen(PORT, () => {
     // Log a message to the console indicating the server is running
     console.log(`Server running at :  http://localhost:${PORT}`)
 })
